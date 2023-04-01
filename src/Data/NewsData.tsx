66eac79edb77d418f40d671f87fb0abe3e31a6a4
Anu <anubusinesscc8@gmail.com>
2023-04-01 13:31:11 +0530

    chore: start build

---
+++ src/Data/NewsData.tsx
@@ -0,0 +1,17 @@
+export const NewsData = {
+  source: {
+    id: "business-insider",
+    name: "Business Insider",
+  },
+  author: "insider@insider.com (Carla Mozée)",
+  title:
+    "Bitcoin edges higher after dismal February for risk assets as investors assess rate outlook and new China data",
+  description:
+    "Bitcoin has surged by more than 40% so far in 2023 and started off March on stronger footing.",
+  url: "https://markets.businessinsider.com/news/currencies/bitcoin-price-crypto-stocks-china-manufacturing-data-risk-assets-copper-2023-3",
+  urlToImage:
+    "https://i.insider.com/63ff5968494c7a0019f21cb0?width=1200&format=jpeg",
+  publishedAt: "2023-03-01T15:48:21Z",
+  content:
+    "Bitcoin pushed higher Wednesday, sustaining its advance alongside other risk assets after the strongest official  manufacturing print in a decade out of China kickstarted trading in March. \r\nThe worl… [+2631 chars]",
+};
